16879f1bb9e8b2e820629e1cee625500143eb0ff
AnjaliKrishna <anjali2sheeja@gmail.com>
2024-11-04 19:39:17 +0530

    create function operate() to select function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
 function add(a,b){
     return a+b;
 }
+
 function subtract(a,b){
     return a-b;
 }
+
 function multiply(a,b){
     return a*b;
 }
+
 function divide(a,b){
     if(b==0){
         alert("Cannot divide by zero!");
@@ -17,9 +20,26 @@ function divide(a,b){
     }
 }
 
+function operate(op1,op2,operator){
+    switch(operator){
+        case '+':
+            result=add(op1,op2);
+            break;
+        case '-':
+            result=subtract(op1,op2);
+            break;
+        case '*':
+            result=multiply(op1,op2)
+            break;
+        case '/':
+            result=divide(op1,op2);
+            break;         
+    }
+    if(result%1!=0)
+        result =result.toFixed(3);
+    return result;
+}
 
-
-let result=divide(2.07,3)
-if(result%1!=0)
-    result =result.toFixed(3);
-console.log(result);
+let op1;
+let operator;
+let op2;
